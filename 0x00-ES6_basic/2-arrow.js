4a39732ed09782cde74689d48e65dea3272ddae9
Thynance <talk2Nnabuiketim@gmail.com>
2024-10-04 14:51:21 +0100

    updated again

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,4 +1,4 @@
-(() => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
@@ -6,4 +6,4 @@
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   })();
-})();
+}
