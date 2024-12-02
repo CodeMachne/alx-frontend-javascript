10901cf3619a2ca7361fde1c417c9d97f308345a
Thynance <talk2Nnabuiketim@gmail.com>
2024-12-03 00:02:02 +0100

    slight changes

--- 0x00-ES6_basic/3-main.js
+++ 0x00-ES6_basic/3-main.js
@@ -1,12 +1,8 @@
 export default function getSumOfHoods(
   initialNumber,
-  expansion1989,
-  expansion2019
+  expansion1989 = 89,
+  expansion2019 = 19
 ) {
-  if (expansion1989 === undefined && expansion2019 === undefined) {
-    expansion1989 = 89;
-    expansion2019 = 19;
-  }
   return initialNumber + expansion1989 + expansion2019;
 }
 console.log(getSumOfHoods(34));
