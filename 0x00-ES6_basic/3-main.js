051cb51011627e7910ea7e0a2efb1c1c43ce7cf1
Thynance <talk2Nnabuiketim@gmail.com>
2024-12-02 23:56:10 +0100

    my commit

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,14 @@
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1989,
+  expansion2019
+) {
+  if (expansion1989 === undefined && expansion2019 === undefined) {
+    expansion1989 = 89;
+    expansion2019 = 19;
+  }
+  return initialNumber + expansion1989 + expansion2019;
+}
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
